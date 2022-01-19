6384ee39dfbeb88252e6e99b75056b5fccdd92d9
Bessong Junior <juniorbesong8@hotmail.com>
2022-01-19 11:40:19 +0100

    Complete the point class with more methods and obj defines making it doing somethg

--- oop/classes_instance.py
+++ oop/classes_instance.py
@@ -36,4 +36,36 @@ an empty class is created with no behavior or data and later assigned 2 instance
 The syntax use here was <object>.<attribute> = <value>.
 '''
 
+#completing the point example
 
+import math
+class Point:
+"Represents a point in two-dimensional geometric coordinates"
+    def __init__(self, x=0, y=0):
+		"""Initialize the position of a new point. The x and y
+		coordinates can be specified. If they are not, the
+		point defaults to the origin."""
+		self.move(x, y)
+
+	def move(self, x, y):
+		"Move the point to a new location in 2D space."
+		self.x = x
+		self.y = y
+
+	def reset(self):
+		"Reset the point back to the geometric origin: 0, 0"
+		self.move(0, 0)
+
+	def calculate_distance(self, other_point):
+		"""Calculate the distance from this point to a second
+		point passed as a parameter.
+		This function uses the Pythagorean Theorem to calculate
+		the distance between the two points. The distance is
+		returned as a float."""
+		return math.sqrt(
+		(self.x - other_point.x) ** 2
+		+ (self.y - other_point.y) ** 2
+		)
+
+
+#Up comming exercise.
